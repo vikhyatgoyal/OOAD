e17524cd67aa19fef5c105786ae5626462302066
Rishi Soni <rishisoni10@users.noreply.github.com>
2018-04-12 10:01:20 -0600

    Created the Memento class

---
+++ OOAD_Project_Repo/src/main/java/com/cub/foodbuddy/model/Memento.java
@@ -0,0 +1,15 @@
+package com.cub.foodbuddy.model;
+
+
+public class Memento
+{
+	private String state;
+	public Memento(String stateToSave)
+	{ 
+		state = stateToSave; 
+	}
+	public String getSavedState()
+	{ 
+		return state; 
+	}
+}
