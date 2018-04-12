f6697d6c1511aeed59667d028fdc96f203e83c72
Rishi Soni <rishisoni10@users.noreply.github.com>
2018-04-12 10:07:38 -0600

    Caretaker class for Memento design pattern

---
+++ OOAD_Project_Repo/src/main/java/com/cub/foodbuddy/model/Caretaker.java
@@ -0,0 +1,16 @@
+package com.cub.foodbuddy.model;
+import java.util.ArrayList;
+
+class Caretaker 
+{
+	private ArrayList<Memento> savedStates = new ArrayList<Memento>();
+	
+	public void addMemento(Memento m)
+	{
+		savedStates.add(m);
+	}
+	public Memento getMemento(int index)
+	{
+		return savedStates.get(index);
+	}
+}
