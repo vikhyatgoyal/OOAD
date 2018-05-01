10a825218a6cebba3a35a25bc1e7644d91ccb726
Rishi Soni <rishisoni10@users.noreply.github.com>
2018-04-30 20:24:25 -0600

    Added Feedback class

---
+++ OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/model/Feedback.java
@@ -0,0 +1,38 @@
+package com.cub.foodbuddy.model;
+
+public class Feedback {
+	
+	/* Attributes */
+	private int id;
+	private String email;
+	private String feedback;
+	private int rating;
+	private String date;
+	private String name;
+	
+	public Feedback() {
+	
+	}
+	
+	public Feedback(String email, String feedback, int rating, String date, String name) {}
+	
+	public String getEmail() {}
+  
+	public void setEmail(String email) {}
+	
+	public String getFeedback() {}
+  
+	public void setFeedback(String feedback) {}
+	
+	public int getRating() {}
+  
+	public void setRating(int rating) {}
+	
+	public String getDate() {}
+  
+	public void setDate(String date) {}
+
+	public String getName() {}
+
+	public void setRestuarantName(String name) {}
+}
