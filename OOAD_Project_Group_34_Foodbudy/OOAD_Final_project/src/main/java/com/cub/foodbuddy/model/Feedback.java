4fd6d1ce6a65e0dd2123c3dc3bfa76f19673bf89
Rishi Soni <rishisoni10@users.noreply.github.com>
2018-04-30 20:39:53 -0600

    Added functionality in Feedback class

--- OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/model/Feedback.java
+++ OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/model/Feedback.java
@@ -1,8 +1,18 @@
 package com.cub.foodbuddy.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Feedback")
 public class Feedback {
 	
 	/* Attributes */
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String email;
 	private String feedback;
@@ -14,25 +24,51 @@ public class Feedback {
 	
 	}
 	
-	public Feedback(String email, String feedback, int rating, String date, String name) {}
+	public Feedback(String email, String feedback, int rating, String date, String name) {
+		this.email = email;
+		this.feedback = feedback;
+		this.rating = rating;
+		this.date = date;
+		this.name = name;
+	}
 	
-	public String getEmail() {}
+	public String getEmail() {
+		return email;
+	}
   
-	public void setEmail(String email) {}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
-	public String getFeedback() {}
+	public String getFeedback() {
+		return feedback;
+	}
   
-	public void setFeedback(String feedback) {}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
 	
-	public int getRating() {}
+	public int getRating() {
+		return rating;
+	}
   
-	public void setRating(int rating) {}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
 	
-	public String getDate() {}
+	public String getDate() {
+		return date;
+	}
   
-	public void setDate(String date) {}
+	public void setDate(String date) {
+		this.date = date;	
+	}
 
-	public String getName() {}
+	public String getName() {
+		return name;
+	}
 
-	public void setRestuarantName(String name) {}
+	public void setRestuarantName(String name) {
+		this.name = name;
+	}
 }
