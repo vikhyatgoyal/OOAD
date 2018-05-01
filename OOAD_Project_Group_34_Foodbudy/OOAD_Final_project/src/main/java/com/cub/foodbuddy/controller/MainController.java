55dbc2544dbae71138042ccb9c795dec101d8e3f
Rishi Soni <rishisoni10@users.noreply.github.com>
2018-04-30 21:14:18 -0600

    Tested addFilter functionality

--- OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/controller/MainController.java
+++ OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/controller/MainController.java
@@ -6,12 +6,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cub.foodbuddy.manager.FilterManager;
+import com.cub.foodbuddy.manager.ProfileManager;
+import com.cub.foodbuddy.model.Filter;
+import com.cub.foodbuddy.model.Profile;
+
+
 @RestController
 public class MainController {
+    @Autowired
+    private ProfileManager profileManager;
+	
+    @Autowired
+    private FilterManager filterManager;
+
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getWelcomeMessage() {
 		return "Welcome to FoodBuddy";
+	}
+	@RequestMapping(value="/endUser/profiles/addFilter/{email}", method = RequestMethod.POST)
+	public void addDefaultEndUserFilter(@RequestBody Filter filter, @PathVariable String email) {
+		EndUser profile = (EndUser) profileManager.getProfileByEmail(email);
+		profileManager.addDefaultEndUserFilter(profile, filter);
+		profileManager.editEndUserProfile(profile, email);
 	}
 	
 }
