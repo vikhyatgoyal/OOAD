19ba24614bfba53fefb98e145c3de5ba3c395b1b
pavandhareshwar <pavansd078@gmail.com>
2018-04-30 20:56:22 -0600

    Added filter manager class
    The filter manager will handle all the filter related queries including the interaction with the DB

---
+++ OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/manager/FilterManager.java
@@ -0,0 +1,46 @@
+package com.cub.foodbuddy.manager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import org.springframework.stereotype.Service;
+
+import com.cub.foodbuddy.model.EndUser;
+import com.cub.foodbuddy.model.Filter;
+
+@SuppressWarnings("deprecation")
+@Service
+public class FilterManager extends Observable{
+	
+	private String email;
+	
+//	private FilterRepository filterRepository;
+//	
+//	@Autowired
+//	public FilterManager(FilterRepository filterRepository) {
+//		this.filterRepository = filterRepository;
+//	}
+	
+//	List<Filter> filters = new ArrayList<>(Arrays.asList(
+//			new Filter(new ArrayList<>(Arrays.asList("Cash")), 
+//					new ArrayList<>(Arrays.asList("Wifi")),
+//					new ArrayList<>(Arrays.asList("Indian", "Mediterranean")),                               
+//					new ArrayList<>(Arrays.asList("")) ,
+//					new ArrayList<>(Arrays.asList("Vegetarian")))
+//			));
+	
+//	public Filter getFilterById(String email){
+//		return filterRepository.findByEmail(email);
+//	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
