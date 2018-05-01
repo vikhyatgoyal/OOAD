76be057a8677dc24eabf70baf73119067c094ce9
vikhyatgoyal <vigo9910@colorado.edu>
2018-04-30 20:49:54 -0600

    Updated Host Class

--- OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/model/Host.java
+++ OOAD_Project_Group_34_Foodbudy/OOAD_Final_project/src/main/java/com/cub/foodbuddy/model/Host.java
@@ -3,26 +3,44 @@ package com.cub.foodbuddy.model;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="Host")
 public class Host extends Profile{
 	/* Attributes */
-
-	private ArrayList<String> feedback;
-	private Map<String,Integer> timeSlots;
+//	private ArrayList<String> promotions;
+//	private ArrayList<String> feedback;
+//	private Map<String,Integer> timeSlots;
 	
 	public int starRatings;
+	@Embedded
+	@Column(name="filter", nullable = true)
 	private Filter services;
+	@Embedded
+	@Column(name="menu", nullable = true)
+	private Menu menu;
 
 	public Host() {
 		super();
 	}
 	
+	public Host(String name, String email, String password, String mobile, String type)
+	{
+		super(name, email, password, mobile, type);
+	}
 	
-
+	public void editProfile(Host hostProfile) {
+		this.setName(hostProfile.getName());
+		this.setMobile(hostProfile.getMobile());
+		this.setPassword(hostProfile.getPassword());
+		
+	}
+	
+//	public void updateHostProfile(Map< String, ArrayList<String> > hostProfile) {
+//		
+//	}
+//	
+//	public void updatePromotion(ArrayList<String> promos) {
+//		
+//	}
+//	
 	public void updateServices(Filter service) {
 		this.services = service;
 	}
@@ -31,6 +49,23 @@ public class Host extends Profile{
 		return this.services;
 	}
 	
+	public void setMenu(Menu menu) {
+		this.menu = menu;
+	}
+	
+	public Menu getMenu() {
+		return this.menu;
+	}
+	
+//	
+//	public void updateAvailability(ArrayList<String> availability) {
+//		
+//	}
+//	
+//	
+//	public void notifyHost(String userTimeSlot, String email) {
+//		
+//	}
 	
 	public void setStarRating(int starRating) {
 		this.starRatings = starRating;
